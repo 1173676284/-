e27ab863b6f3aabd373bd245e9410ce4342e37e1
徐迪华 <xudihua@1deMacBook-Pro.local>
2017-11-30 10:53:37 +0800

    Initial Commit

---
+++ zhonxinV2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhonxinV2
+//
+//  Created by 徐迪华 on 2017/11/30.
+//  Copyright © 2017年 徐迪华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhonxinV2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhonxinV2
+//
+//  Created by 徐迪华 on 2017/11/30.
+//  Copyright © 2017年 徐迪华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
